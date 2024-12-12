815fba49457cdae060e0c016ab2ea9f208cf6350
Michael Hosamani <michaelhosamani26@gmail.com>
2024-12-12 18:02:17 +0530

    added signup and login pages

--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -2,7 +2,12 @@
 
 export function Signup(){
 
-    return <div>
-        Signup
+    return <div className="h-screen w-screen flex items-center justify-center bg-gray-100">
+        <div className="w-60 p-6 flex flex-col gap-4 items-center border rounded-lg shadow-lg bg-white">
+            <h2 className="text-purple-300">Sign-up</h2>
+            <input type="text" placeholder="username" className="p-2 border placeholder-purple-300 rounded-lg"/>
+            <input type="text" placeholder="password" className="p-2 border placeholder-purple-300 rounded-lg" />
+            <button className="border w-full p-2 bg-purple-300 rounded-lg" >Submit</button>
+        </div>
     </div>
 }
