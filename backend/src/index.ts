9c9db712d8033fc9171a4be7df189dafb673ab28
Michael Hosamani <michaelhosamani26@gmail.com>
2025-01-09 15:52:28 +0530

    changed directory for serving frontend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -40,10 +40,10 @@ app.use("/api/v1/users", userRouter)
 app.use("/api/v1/messages", messageRouter)
 
 if(process.env.NODE_ENV === "production"){
-  app.use(express.static(path.join(__dirname, "../frontend/dist")))
+  app.use(express.static(path.join(__dirname, "../../frontend/dist")))
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
+    res.sendFile(path.join(__dirname, "../../frontend", "dist", "index.html"))
   })
 }
 
